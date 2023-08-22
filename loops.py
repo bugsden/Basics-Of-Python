0453dff25e3134d5faa7082d2ad8e56db12eb611
bugsden <142705721+bugsden@users.noreply.github.com>
2023-08-22 07:14:38 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,14 @@
+#This while loop reads and add numbers from 0 through 9 including 9
+
+sum = 0
+i = 0
+while i<10:
+    sum = sum + i
+    print(sum)
+    i = i + 1
+
+#This for loop reads and add numbers from 0 through 9 including 9
+
+for i in range(10):
+    sum = sum +i 
+    print(sum)
